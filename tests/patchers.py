b4dda9742631121df361adf0ab9defc4d078f919
Jeff Irion <jefflirion@users.noreply.github.com>
2019-09-17 06:28:23 -0700

    Add tests for TcpHandle

--- tests/patchers.py
+++ tests/patchers.py
@@ -1,3 +1,4 @@
+import select
 import socket
 
 from mock import patch
@@ -10,8 +11,24 @@ class FakeSocket(object):
     def close(self):
         pass
 
-    def recv(self, numbytes):
+    def recv(self, bufsize):
         pass
 
     def send(self, data):
         pass
+
+    def setblocking(self, *args, **kwargs):
+        pass
+
+
+patch_create_connection = patch('socket.create_connection', return_value=FakeSocket())
+
+def patch_recv(response):
+    def _recv(self, bufsize):
+        return response
+
+    return patch('{}.FakeSocket.recv'.format(__name__), _recv)
+
+patch_select_success = patch('select.select', return_value=(True, True, True))
+
+patch_select_fail = patch('select.select', return_value=(False, False, False))
--- tests/test_tcp_handle.py
+++ tests/test_tcp_handle.py
@@ -1,24 +1,52 @@
 from mock import patch
 import unittest
 
-from adb_shell.tcp_handle import TcpHandle
+from adb_shell.tcp_handle import TcpHandle, TcpTimeoutException
+
+from . import patchers
 
 
 class TestAdbHandle(unittest.TestCase):
     def setUp(self):
-        """TODO
+        """Create a ``TcpHandle`` and connect to a TCP service.
 
         """
         self.handle = TcpHandle('IP:PORT')
+        with patchers.patch_create_connection:
+            self.handle.connect()
 
-    def test_init(self):
+    def tearDown(self):
+        """Close the socket connection."""
+        self.handle.close()
+
+    def test_bulk_read(self):
         """TODO
 
         """
-        self.assertTrue(True)
+        with patchers.patch_recv(b'TEST'), patchers.patch_select_success:
+            self.assertEqual(self.handle.bulk_read(1234), b'TEST')
 
-    def test_connect_success(self):
+        with patchers.patch_select_fail:
+            with self.assertRaises(TcpTimeoutException):
+                self.handle.bulk_read(b'FAIL')
+
+    def test_bulk_write(self):
         """TODO
 
         """
-        self.assertTrue(self.handle.connect())
+        with patchers.patch_select_success:
+            self.handle.bulk_write(b'TEST')
+
+        with patchers.patch_select_fail:
+            with self.assertRaises(TcpTimeoutException):
+                self.handle.bulk_write(b'FAIL')
+
+
+class TestAdbHandle2(TestAdbHandle):
+    def setUp(self):
+        """Create a ``TcpHandle`` and connect to a TCP service.
+
+        """
+        self.handle = TcpHandle('IP')
+        with patchers.patch_create_connection:
+            self.handle.connect()
