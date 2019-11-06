6dc9983f72b3307c835b5d81eb7dc2d7f7ff72f7
Jeff Irion <jefflirion@users.noreply.github.com>
2019-11-05 19:56:13 -0800

    Bump version to 0.0.8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2019, Jeff Irion'
 author = 'Jeff Irion'
 
 # The short X.Y version
-version = '0.0.7'
+version = '0.0.8'
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
