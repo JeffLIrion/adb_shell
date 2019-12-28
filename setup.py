763717a134280cde97a6896a30512cc3e1153add
Jeff Irion <JeffLIrion@users.noreply.github.com>
2019-12-27 17:03:53 -0800

    Add more info to project description (#66)
    * Add more info to project description
    
    * Add URL

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='adb_shell',
     version='0.1.0',
-    description='ADB shell functionality',
+    description='A Python implementation of ADB with shell and FileSync functionality.',
+    long_description=readme,
+    url='https://github.com/JeffLIrion/adb_shell',
     author='Jeff Irion',
     author_email='jefflirion@users.noreply.github.com',
     packages=['adb_shell', 'adb_shell.auth', 'adb_shell.handle'],
