865d72176e6fdbddadceea2dc83c818bfcfd1c63
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-04-22 07:45:28 -0700

    Add 'from_int()' function to test_adb_message.py (#87)

--- tests/test_adb_message.py
+++ tests/test_adb_message.py
@@ -8,6 +8,10 @@ from adb_shell.adb_device import AdbDevice
 from adb_shell.adb_message import AdbMessage, checksum, unpack
 
 
+def from_int(n):
+    return ''.join(chr((n >> (i * 8)) % 256) for i in range(4)).encode('utf-8')
+
+
 class TestAdbMessage(unittest.TestCase):
     def test_checksum_bytearray(self):
         cs = checksum(bytearray('TEST', 'utf-8'))
@@ -24,3 +28,7 @@ class TestAdbMessage(unittest.TestCase):
     def test_unpack_error(self):
         with self.assertRaises(ValueError):
             unpack(b'TEST')
+
+    def test_constants(self):
+        for key, val in constants.ID_TO_WIRE.items():
+            self.assertEqual(key, from_int(val))
