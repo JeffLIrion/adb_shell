87b20f36b23e2a5df187951d0c2d7b853d675ccf
Jeff Irion <jefflirion@users.noreply.github.com>
2020-06-14 15:21:57 -0700

    Alphabetize attributes

--- adb_shell/hidden_helpers.py
+++ adb_shell/hidden_helpers.py
@@ -99,11 +99,13 @@ class _AdbTransactionInfo(object):  # pylint: disable=too-few-public-methods
         The ID for the sender (i.e., the device running this code)
     remote_id : int
         The ID for the recipient
-    transport_timeout_s : float, None
-        Timeout in seconds for sending and receiving packets, or ``None``; see :meth:`BaseTransport.bulk_read() <adb_shell.transport.base_transport.BaseTransport.bulk_read>`
-        and :meth:`BaseTransport.bulk_write() <adb_shell.transport.base_transport.BaseTransport.bulk_write>`
     total_timeout_s : float
         The total time in seconds to wait for a command in ``expected_cmds`` in :meth:`AdbDevice._read`
+    transport_timeout_s : float, None
+        Timeout in seconds for sending and receiving packets, or ``None``; see :meth:`BaseTransport.bulk_read() <adb_shell.transport.base_transport.BaseTransport.bulk_read>`,
+        :meth:`BaseTransport.bulk_write() <adb_shell.transport.base_transport.BaseTransport.bulk_write>`,
+        :meth:`BaseTransportAsync.bulk_read() <adb_shell.transport.base_transport_async.BaseTransportAsync.bulk_read>`, and
+        :meth:`BaseTransportAsync.bulk_write() <adb_shell.transport.base_transport_async.BaseTransportAsync.bulk_write>`,
 
     """
     def __init__(self, local_id, remote_id, transport_timeout_s=None, total_timeout_s=constants.DEFAULT_TOTAL_TIMEOUT_S):
