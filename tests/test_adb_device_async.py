90917ec0cbfe9a0a19c40f54ee7be1b6c6f0b359
Attila KÓBOR <Attila.KOBOR@nng.com>
2020-06-16 16:21:20 +0200

    Fix chunk data dependent test cases, to use the new property instead of hardcoded value.

--- tests/test_adb_device_async.py
+++ tests/test_adb_device_async.py
@@ -635,7 +635,7 @@ class TestAdbDeviceAsync(unittest.TestCase):
         self.device._transport._bulk_write = b''
 
         mtime = 100
-        filedata = b'0' * int(3.5 * constants.MAX_PUSH_DATA)
+        filedata = b'0' * int(3.5 * self.device.max_chunk_size)
 
         # Provide the `bulk_read` return values
         self.device._transport._bulk_read = join_messages(AdbMessage(command=constants.OKAY, arg0=1, arg1=1, data=b'\x00'),
@@ -646,12 +646,12 @@ class TestAdbDeviceAsync(unittest.TestCase):
                                                           AdbMessage(command=constants.CLSE, arg0=1, arg1=1, data=b''))
 
         # Expected `bulk_write` values
-        mpd0, mpd1, mpd2, mpd3 = 0, constants.MAX_PUSH_DATA, 2*constants.MAX_PUSH_DATA, 3*constants.MAX_PUSH_DATA
+        mpd0, mpd1, mpd2, mpd3 = 0, self.device.max_chunk_size, 2*self.device.max_chunk_size, 3*self.device.max_chunk_size
         expected_bulk_write = join_messages(AdbMessage(command=constants.OPEN, arg0=1, arg1=0, data=b'sync:\x00'),
                                             AdbMessage(command=constants.WRTE, arg0=1, arg1=1, data=join_messages(FileSyncMessage(command=constants.SEND, data=b'/data,33272'),
-                                                                                                                  FileSyncMessage(command=constants.DATA, data=filedata[mpd0:mpd1]))),
-                                            AdbMessage(command=constants.WRTE, arg0=1, arg1=1, data=join_messages(FileSyncMessage(command=constants.DATA, data=filedata[mpd1:mpd2]))),
-                                            AdbMessage(command=constants.WRTE, arg0=1, arg1=1, data=join_messages(FileSyncMessage(command=constants.DATA, data=filedata[mpd2:mpd3]),
+                                                                                                                  FileSyncMessage(command=constants.DATA, data=filedata[mpd0:mpd1]),
+                                                                                                                  FileSyncMessage(command=constants.DATA, data=filedata[mpd1:mpd2]),
+                                                                                                                  FileSyncMessage(command=constants.DATA, data=filedata[mpd2:mpd3]),
                                                                                                                   FileSyncMessage(command=constants.DATA, data=filedata[mpd3:]),
                                                                                                                   FileSyncMessage(command=constants.DONE, arg0=mtime))),
                                             AdbMessage(command=constants.OKAY, arg0=1, arg1=1),
