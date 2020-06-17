55ef1d2954fa0023907bf8cc2a48944e34323c39
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-06-16 21:08:22 -0700

    Remove period

--- adb_shell/adb_device_async.py
+++ adb_shell/adb_device_async.py
@@ -98,7 +98,7 @@ class AdbDeviceAsync(object):
     _banner : bytearray, bytes
         The hostname of the machine where the Python interpreter is currently running
     _maxdata: int
-        Maximum amount of data in an ADB packet.
+        Maximum amount of data in an ADB packet
     _transport : BaseTransportAsync
         The transport that is used to connect to the device; must be a subclass of :class:`~adb_shell.transport.base_transport_async.BaseTransportAsync`
 
