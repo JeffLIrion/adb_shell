7165dfd2d52c920750e39513e836a2b1e478f1aa
Jeff Irion <JeffLIrion@users.noreply.github.com>
2020-12-19 09:00:35 -0800

    Bump the version to 0.3.0 (#140)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,9 +26,9 @@ copyright = '2020, Jeff Irion and contributors'
 author = 'Jeff Irion'
 
 # The short X.Y version
-version = '0.2.3'
+version = '0.3.0'
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
