5f5796ec7619f87471d5aca55e301c8f6e65b87e
Jeff Irion <jefflirion@users.noreply.github.com>
2020-12-19 08:55:38 -0800

    Bump the version to 0.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='adb_shell',
-    version='0.2.3',
+    version='0.3.0',
     description='A Python implementation of ADB with shell and FileSync functionality.',
     long_description=readme,
     keywords=['adb', 'android'],
